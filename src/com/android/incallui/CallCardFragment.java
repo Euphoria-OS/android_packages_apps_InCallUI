de7969625ca12c362461b751988da8c2f56c2436
Matt Garnes <matt@cyngn.com>
2015-03-27 18:05:42 -0700

    Allow hiding volume boost feature with overlay.
    Some devices do not support it, or do not support it well. Allow us to
    hide it with an overlayable config flag.
    
    Change-Id: I2c3b0b5d3a6e738b311b365f5855c41f7d08ab9a

--- src/com/android/incallui/CallCardFragment.java
+++ src/com/android/incallui/CallCardFragment.java
@@ -1156,8 +1156,14 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
         }
     }
 
+    private boolean isVBHiddenByOverride() {
+        return getResources().getBoolean(R.bool.config_disable_audio_boost);
+    }
+
     private void updateVBButton() {
-        if (isVBAvailable()
+        if (isVBHiddenByOverride()) {
+            mVBButton.setVisibility(View.INVISIBLE);
+        } else if (isVBAvailable()
                 && mAudioManager.getParameters(VOLUME_BOOST).contains("=on")) {
 
                 mVBButton.setBackgroundResource(R.drawable.vb_active);
@@ -1218,7 +1224,7 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
     private void updateVBbyCall(int state) {
         updateVBButton();
 
-        if (Call.State.ACTIVE == state) {
+        if (Call.State.ACTIVE == state && !isVBHiddenByOverride()) {
             mVBButton.setVisibility(View.VISIBLE);
         } else if (Call.State.DISCONNECTED == state) {
             if (!CallList.getInstance().hasAnyLiveCall()
